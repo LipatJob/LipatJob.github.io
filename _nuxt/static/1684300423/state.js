e8d657bc0813d68c379d73859fa21d751803a80c
LipatJob <LipatJob@users.noreply.github.com>
2023-05-17 05:14:35 +0000

    deploy: c4568d868dec65ea065c8440a7dff6b0e882350b

--- _nuxt/static/1684300423/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1684300423",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"afcdb53a"}}}}(null,"\u002F"));
